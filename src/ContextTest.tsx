c9f6b5e3fc3cccb3044677819cf61ab892a6e9e4
yk <read0more@gmail.com>
2021-04-18 16:04:52 +0900

    context: context 하나 더 만들어 분리하여 불필요한 리렌더링 제거

--- src/ContextTest.tsx
+++ src/ContextTest.tsx
@@ -1,24 +1,22 @@
 import React, { createContext, useState, useContext } from "react";
 
 type UserType = {
-  user: {
-    username: string;
-  } | null;
-  setUser: React.Dispatch<React.SetStateAction<{ username: string }>> | null;
+  username: string;
 };
 
-const UserContext = createContext<UserType>({
-  user: null,
-  setUser: null,
-});
+type UserUpdateType = React.Dispatch<React.SetStateAction<UserType>> | null;
+
+const UserContext = createContext<UserType>({ username: "" });
+const UserUpdateContext = createContext<UserUpdateType>(null);
 
 function UserProvider({ children }: { children: JSX.Element[] }) {
   const defaultUser = { username: "" };
-  const [user, setUser] = useState(defaultUser);
-
+  const [user, setUser] = useState<UserType>(defaultUser);
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
+    <UserContext.Provider value={user}>
+      <UserUpdateContext.Provider value={setUser}>
+        {children}
+      </UserUpdateContext.Provider>
     </UserContext.Provider>
   );
 }
@@ -27,15 +25,19 @@ function useUser() {
   return useContext(UserContext);
 }
 
+function useUserUpdate() {
+  return useContext(UserUpdateContext);
+}
+
 function UserInfo() {
-  const { user } = useUser();
+  const user = useUser();
   if (!user) return <div>사용자 정보가 없습니다.</div>;
   return <div>{user.username}</div>;
 }
 
 function Authenticate() {
   console.log("Authenticate 렌더링");
-  const { setUser } = useUser();
+  const setUser = useUserUpdate();
   const onClick = () => {
     if (!setUser) return;
     setUser({ username: "velopert" });
